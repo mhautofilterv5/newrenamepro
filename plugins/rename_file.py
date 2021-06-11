63f473199e1042b79c0add72a28b981ba6fbfc01
mhautofilterv5 <85279476+mhautofilterv5@users.noreply.github.com>
2021-06-11 20:17:24 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -63,11 +63,11 @@ async def rename_doc(bot, update):
                await update.reply_text(" Sorry, You are **B A N N E D**")
                return
         except UserNotParticipant:
-            #await update.reply_text(f"Join @{update_channel} To Use Me")
+            #await update.reply_text(f"Join @malluhubbmovies And support")
             await update.reply_text(
                 text="**Please Join My Update Channel Before Using Me..**",
                 reply_markup=InlineKeyboardMarkup([
-                    [ InlineKeyboardButton(text="🔔Join My Updates Channel🔔", url=f"https://t.me/{update_channel}")]
+                    [ InlineKeyboardButton(text="🖤Join My Updates Channel🖤", url=f"https://t.me/MALLUZHUB")]
               ])
             )
             return
